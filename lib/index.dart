4e8a68d6020f4b194e5baea7aa08a62cfa7bb82c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-20 14:16:04 +0000

    commit tiago

--- lib/index.dart
+++ lib/index.dart
@@ -1,4 +1,4 @@
 // Export pages
 export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/login/login_widget.dart' show LoginWidget;
-export '/admin/admin_widget.dart' show AdminWidget;
+export '/pages/login/login_widget.dart' show LoginWidget;
+export '/pages/admin/admin_widget.dart' show AdminWidget;
